79eb281da02d9da28d7b383f5605fdee586d55d3
Melinda-haw <melinda@horseandwater.com.au>
2024-05-21 09:28:12 +1000

    Added loop-content template functionality and commented out sidebar template

--- archive.php
+++ archive.php
@@ -31,7 +31,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/loop-content', get_post_type() );
 
 			endwhile;
 
@@ -47,5 +47,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
