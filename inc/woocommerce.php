d49240865f3c8245ea1124b1afbe2a962172ee19
Melinda-haw <melinda@horseandwater.com.au>
2024-11-06 10:05:44 +1000

    Updated default Woocommerce thumbnail size

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -21,7 +21,7 @@ function haw_starter_woocommerce_setup() {
 		'woocommerce',
 		array(
 			'thumbnail_image_width' => 150,
-			'single_image_width'    => 300,
+			'single_image_width'    => 768,
 			'product_grid'          => array(
 				'default_rows'    => 3,
 				'min_rows'        => 1,
